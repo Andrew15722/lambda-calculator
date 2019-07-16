cfb7a10614769399135acea08b9e92abd88b59be
andrew-mendez <andrew15722@yahoo.com>
2019-07-16 14:28:16 -0700

    added scss to project, got the numbers.js and number button.js components to show on the app. still working on the other data components.

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React from 'react';
 
-const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+const Display = ({ number }) => {
+	return <div className="display">{/* Display any props data here */ 0}</div>;
 };
+
+export default Display;
